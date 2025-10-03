a20ddf0fb20413daa06409e9b0b1c2f8c9c05c03
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-03 11:47:31 -0300

    testando o recurso de botao inserir

--- src/view/Clientes/index.php
+++ src/view/Clientes/index.php
@@ -19,6 +19,9 @@
                 <div class="container-fluid px-4">
                     <h1 class="mt-4">CLIENTES</h1>
                 </div>
+                <div>
+                <button type="button" class="p=1 btn btn-dark">Inserir +</button>
+                </div>
 
                 <table class="table align-middle mb-0 bg-white">
                     <thead class="bg-light">
