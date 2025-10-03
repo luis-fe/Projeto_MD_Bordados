c0f4c3ebc88f10f29cfe67ffe989b8ff46a16757
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-03 11:17:47 -0300

    TESTANDO A ENTRADA NO CADASTRO DE CLIENTES

--- src/view/Clientes/index.php
+++ src/view/Clientes/index.php
@@ -5,7 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CADASTRO CLIENTES</title>
 </head>
-<body>
+<body class="sb-nav-fixed">
+    <?php include '../../../MenuSuspenso.php'; ?>
+    <div id="layoutSidenav">
+        <?php include '../../../MenuLateral.php'; ?>
+
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">CLIENTES</h1>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="/view/assets/demo/chart-area-demo.js"></script>
+    <script src="/view/assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+
     
 </body>
 </html>
