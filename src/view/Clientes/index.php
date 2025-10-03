43545fb18991351ea5bd51cb0ba93e67a0d61c35
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-03 11:28:05 -0300

    correcoes

--- src/view/Clientes/index.php
+++ src/view/Clientes/index.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CADASTRO CLIENTES</title>
+    <title>MD - BORDADO E REVISAO</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+    <link href="styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 <body class="sb-nav-fixed">
     <?php include '../../../MenuSuspenso.php'; ?>
